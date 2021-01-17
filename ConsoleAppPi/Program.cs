11da8e75ca245e3a50be1a5d01334fa7f0ededfd
anpt1992 <pham.an.20.09@gmail.com>
2021-01-17 22:45:29 +0700

    Add project files.

---
+++ ConsoleAppPi/Program.cs
@@ -0,0 +1,22 @@
+﻿using Iot.Device.CpuTemperature;
+using System;
+using System.Threading;
+
+namespace ConsoleAppPi
+{
+    class Program
+    {
+        static CpuTemperature temperature = new CpuTemperature();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                if (temperature.IsAvailable)
+                {
+                    Console.WriteLine($"The CPU temperature is {temperature.Temperature.DegreesCelsius}");
+                }
+                Thread.Sleep(2000); // sleep for 2000 milliseconds, 2 seconds
+            }
+        }
+    }
+}
